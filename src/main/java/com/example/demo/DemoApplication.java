ad98d5dff17b00566098cb245f7fbfc41fd9b377
来朋 <lprebok@163.com>
2019-04-09 14:19:41 +0800

    IOC注入

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(DemoApplication.class, args);
-		MyServiceDao md=new MyServiceDaoimpl();
+		ApplicationContext ac=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		MyServiceDao md=(MyServiceDao)ac.getBean("MyServiceDao");
 		md.save();
 	}
 
--- src/main/java/com/example/demo/MyServiceDaoimpl.java
+++ src/main/java/com/example/demo/MyServiceDaoimpl.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
 public class MyServiceDaoimpl implements MyServiceDao {
-
 	public void save() {
 		System.out.println("保存数据成功！");
 	}
 	
-	
-	
+
 }
