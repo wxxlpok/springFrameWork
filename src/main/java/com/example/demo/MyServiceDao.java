45e95f0af7ace80b44a77536ed21669def90ad28
来朋 <lprebok@163.com>
2019-04-09 12:08:34 +0800

    开始测试IOC

---
+++ src/main/java/com/example/demo/MyServiceDao.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public interface MyServiceDao {
+
+	public void save();
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/MyServiceDaoimpl.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class MyServiceDaoimpl implements MyServiceDao {
+
+	public void save() {
+		System.out.println("保存数据成功！");
+	}
+	
+	
+	
+}
