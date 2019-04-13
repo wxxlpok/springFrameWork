bf0385c904b2c96e3da4d0d4e22b042f8b83049f
来朋 <lprebok@163.com>
2019-04-13 09:00:23 +0800

    注解

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(DemoApplication.class, args);
 		ApplicationContext ac=new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		MyServiceDao md=(MyServiceDao)ac.getBean("MyServiceDao");
+		MyServiceDao md=(MyServiceDao)ac.getBean("myservicedao");
 		md.save();
 	}
 
--- src/main/java/com/example/demo/MyServiceDaoimpl.java
+++ src/main/java/com/example/demo/MyServiceDaoimpl.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component(value="myservicedao")
 public class MyServiceDaoimpl implements MyServiceDao {
 	public void save() {
 		System.out.println("保存数据成功！");
